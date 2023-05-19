bfbfd7afe36eab0b7e3e666924b5f0dcb2db1eed
ben <ben@bnsmcx.com>
2023-05-19 12:59:06 +0700

    Implement synchronization, Bot.Trade(), Bot.UpdateScoreboard()

--- bot/bot.go
+++ bot/bot.go
@@ -1,12 +1,33 @@
 package bot
 
+import (
+	"sync"
+	"time"
+)
+
+type Summary struct {
+	Cash float64
+}
+
 type Bot struct {
 	Name     string
 	Cash     float64
 	LongWin  int
 	ShortWin int
+	Mu       *sync.Mutex
+	SB       map[string]Summary
 }
 
-func (b Bot) trade() {
+func (b Bot) Trade() {
+	for i := 0; i < 5; i++ {
+		b.Cash++
+		b.UpdateScoreboard()
+		time.Sleep(time.Second)
+	}
+}
 
+func (b Bot) UpdateScoreboard() {
+	b.Mu.Lock()
+	b.SB[b.Name] = Summary{b.Cash}
+	b.Mu.Unlock()
 }
--- main.go
+++ main.go
@@ -3,21 +3,32 @@ package main
 import (
 	"log"
 	"solid-eureka/bot"
+	"sync"
+	"time"
 )
 
+var scoreboard = make(map[string]bot.Summary)
+var mu sync.Mutex
+
 var bots = []bot.Bot{
-	{"Alpha", 100, 138, 14},
-	{"Bravo", 100, 59, 12},
-	{"Charlie", 100, 109, 2},
-	{"Delta", 100, 71, 2},
-	{"Echo", 100, 71, 15},
-	{"Foxtrot", 100, 109, 15},
-	{"Golf", 100, 61, 15},
-	{"Average", 100, 95, 11},
+	{"Alpha", 100, 138, 14, &mu, scoreboard},
+	{"Bravo", 100, 59, 12, &mu, scoreboard},
+	{"Charlie", 100, 109, 2, &mu, scoreboard},
+	{"Delta", 100, 71, 2, &mu, scoreboard},
+	{"Echo", 100, 71, 15, &mu, scoreboard},
+	{"Foxtrot", 100, 109, 15, &mu, scoreboard},
+	{"Golf", 100, 61, 15, &mu, scoreboard},
+	{"Average", 100, 95, 11, &mu, scoreboard},
 }
 
 func main() {
 	for _, b := range bots {
-		log.Println(b)
+		go b.Trade()
 	}
+	time.Sleep(time.Second * time.Duration(7))
+	mu.Lock()
+	for k, v := range scoreboard {
+		log.Println(k, v)
+	}
+	mu.Unlock()
 }
